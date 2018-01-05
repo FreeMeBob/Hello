bd5dd6e18c022565c721c8dea471a712447fbcdd
YifanWang_Bob <bob.wang@kikatech.com>
2018-01-05 11:43:12 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int num=sc.nextInt();
+        for (int i=0;i<num;i++)
+        {
+            print();
+        }
+    }
+    static void print()
+    {
+        System.out.println("Hello");
+    }
+}
